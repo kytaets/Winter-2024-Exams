564073f836bbf54be2afe482893adffa6143f7d5
kytaec <vladislavtokovenko4@gmail.com>
2024-01-13 18:28:52 +0200

    removed unnecessary argument

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-const incNumbers = (object, ...rest_variables) => {
+const incNumbers = (object) => {
   for (delete_file in object) {
     if ((typeof object[delete_file]).charAt(0).toUpperCase() === 'N') {
       object[delete_file] = object[delete_file] + 1;
