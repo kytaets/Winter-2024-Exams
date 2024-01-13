79eed7ac238639849fadecfa9fd6a29106414134
kytaec <vladislavtokovenko4@gmail.com>
2024-01-13 12:41:19 +0200

    renamed constant

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -3,8 +3,8 @@
 'use strict';
 
 const distinct = (data) => {
-  const A = new Set(data);
-  return Array.from(A);
+  const dataSet = new Set(data);
+  return Array.from(dataSet);
 };
 
 module.exports = distinct;
