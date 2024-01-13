8cae77c1a0bbfc20d5d41a3d3e87448e7002e748
kytaec <vladislavtokovenko4@gmail.com>
2024-01-13 18:51:22 +0200

    removed unused braces

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -5,21 +5,15 @@
 const longest = (object) => {
   let s = 'Not found';
   for (const i of object) {
-    {
-      {
-        if (i.length > x) {
-          x = i.length;
-        } else {
-          i = 0;
-        }
-      }
-      {
-        if (i.length >= x) {
-          s = i;
-        } else {
-          i = -1;
-        }
-      }
+    if (i.length > x) {
+      x = i.length;
+    } else {
+      i = 0;
+    }
+    if (i.length >= x) {
+      s = i;
+    } else {
+      i = -1;
     }
   }
   Object.assign(line, {length: s.length});
