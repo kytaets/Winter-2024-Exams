d72bb86330976df56d143eee5998616632edf1e9
kytaec <vladislavtokovenko4@gmail.com>
2024-01-13 12:43:08 +0200

    removed unnecessary line

--- Tasks/count.js
+++ Tasks/count.js
@@ -5,7 +5,6 @@
 const count = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
-  // eslint-disable-next-line no-restricted-syntax
   for (const key of keys) {
     const value = obj[key];
     if (typeof value === 'number') {
