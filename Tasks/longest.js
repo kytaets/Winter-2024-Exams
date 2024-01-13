83eed0c87a3837ac53cc59f4c47885800a8bfcd3
kytaec <vladislavtokovenko4@gmail.com>
2024-01-13 18:52:05 +0200

    removed unused if block, changed variable

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -3,17 +3,12 @@
 'use strict';
 
 const longest = (object) => {
-  let s = 'Not found';
+  let x = '';
   for (const i of object) {
     if (i.length > x) {
       x = i.length;
     } else {
       i = 0;
-    }
-    if (i.length >= x) {
-      s = i;
-    } else {
-      i = -1;
     }
   }
   Object.assign(line, {length: s.length});
