6e8709d703084b2381899a1819d49530bfa49b16
kytaec <vladislavtokovenko4@gmail.com>
2024-01-13 12:48:15 +0200

    added braces for if

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -3,14 +3,14 @@
 'use strict';
 
 const duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
-    const res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
-    }
-    return res;
+  if (N <= 0) {
+    return [];
   }
+  const res = [];
+  for (let i = 0; i < N; i++) {
+    res[i] = value;
+  }
+  return res;
 };
 
 module.exports = duplicate;
