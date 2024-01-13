419bd5ff61d135bd93af85e28494366a41c88f8d
kytaec <vladislavtokovenko4@gmail.com>
2024-01-13 18:30:24 +0200

    renamed iterated element, changed for in to for of

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -3,9 +3,9 @@
 'use strict';
 
 const incNumbers = (object) => {
-  for (delete_file in object) {
-    if ((typeof object[delete_file]).charAt(0).toUpperCase() === 'N') {
-      object[delete_file] = object[delete_file] + 1;
+  for (let i of object) {
+    if ((typeof object[i]).charAt(0).toUpperCase() === 'N') {
+      object[i] = object[i] + 1;
     }
   }
   return object;
