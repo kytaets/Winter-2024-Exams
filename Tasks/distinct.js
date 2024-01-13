5a5fd328a479c475365497d7f8e57bddc3cf51c8
kytaec <vladislavtokovenko4@gmail.com>
2024-01-13 12:40:21 +0200

    removed unnecessary lines, added converting from Set to Array

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -4,16 +4,7 @@
 
 const distinct = (data) => {
   const A = new Set(data);
-  let w = 0;
-  data.forEach((a) => {
-    if (A.has(a)) {
-      delete data[w];
-    } else {
-      A.add(a);
-    }
-    w++;
-  });
-  return data;
+  return Array.from(A);
 };
 
 module.exports = distinct;
