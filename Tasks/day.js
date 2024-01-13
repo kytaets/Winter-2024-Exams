367b2621dc81cc0483aecc3140431b03f2473a0e
kytaec <vladislavtokovenko4@gmail.com>
2024-01-13 12:03:40 +0200

    changed array's name

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,12 +2,12 @@
 
 'use strict';
 
-const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (s) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < days.length; i++) {
+    if (s.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
   }
