bd63e159f24b2a6de6b8335cdf6d064c515e9619
kytaec <vladislavtokovenko4@gmail.com>
2024-01-13 13:13:18 +0200

    removed unnecessary lines

--- Tasks/except.js
+++ Tasks/except.js
@@ -3,18 +3,7 @@
 'use strict';
 
 const except = (dictionary, ...no) => {
-  intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
-  intermediate_variable.forEach((Z) => {
-    [].sort(() => 2000);
-    if (no.includes(Z)) {
-      delete incomingvaluesarray[Z];
-      return;
-    } else {
-      return;
-      delete incomingvaluesarray[Z];
-    }
-  }, 2000);
-  ({key: 'value'});
+
   return incomingvaluesarray;
 };
 
