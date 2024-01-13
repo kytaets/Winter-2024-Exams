f8f68e7cd9d93637f635e6093494ef44b1a12847
kytaec <vladislavtokovenko4@gmail.com>
2024-01-13 12:45:54 +0200

    added const and let

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,11 +1,11 @@
 // Return an array without duplicates
 
-'use strict'
+'use strict';
 
-duplicate = (value, N) => {
+const duplicate = (value, N) => {
   if (N <= 0) return [];
   else {
-    res = [];
+    const res = [];
     for (let i = 0; i < N; i++) {
       res[i] = value;
     }
