8cc3305524580fdec313de402a09c82b8e721d7a
kytaec <vladislavtokovenko4@gmail.com>
2024-01-13 11:52:24 +0200

    changed forEach to for of, added brackets for if statement

--- Tasks/count.js
+++ Tasks/count.js
@@ -3,11 +3,13 @@
 const count = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key of keys) {
     const value = obj[key];
-    if (typeof value === 'number')
+    if (typeof value === 'number') {
       sum += value;
-  });
+    }
+  }
   return sum;
 };
 
